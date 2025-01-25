d15068d0003b35162ebef9db96f0164e623d31a6
COOLSTORM36 <yoyo.sunshine@hotmail.com>
2025-01-25 23:56:11 +0000

    #

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
+import { createElement } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root = createRoot(document.getElementById('root'))
+const reactElement = createElement('h1', null, 'Hello, world!')
+
+root.render(
+  reactElement
 )
