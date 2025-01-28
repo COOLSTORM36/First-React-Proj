78aa4efb16cc1b54889d9806114fa4b995ea71a8
COOLSTORM36 <yoyo.sunshine@hotmail.com>
2025-01-28 17:20:18 +0000

    #

---
+++ src/MyTitle.jsx
@@ -0,0 +1,10 @@
+import Header from './Header'
+
+export default function MyTitle() {
+    return (
+      <div>
+        <Header />
+        <h1>Fun fac about react</h1>
+      </div>
+    )
+  }
--- src/main.jsx
+++ src/main.jsx
@@ -2,33 +2,11 @@
 // import { createElement } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
+import MyTitle from './MyTitle'
 import App from './App.jsx'
 
 const root = createRoot(document.getElementById('root'))
-
-function Header() {
-  return (
-    <header className='header'>
-      <img src="src/assets/react.svg" width={40} />
-      <nav>
-        <ul className="nav-list">
-          <li className='nav-list-item'>Pricing</li>
-          <li className='nav-list-item'>About</li>
-          <li className='nav-list-item'>Contact</li>
-        </ul>
-      </nav>
-    </header>
-  )
-}
-
-function MyTitle() {
-  return (
-    <div>
-      <Header />
-      <h1>Fun fact about react</h1>
-    </div>
-  )
-}
 
 function MyList() {
   return (
