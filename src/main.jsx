00a4c59fa1bf713caad15005fc7dddeb8b6791f5
COOLSTORM36 <yoyo.sunshine@hotmail.com>
2025-01-28 17:24:52 +0000

    #

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,2 @@
-// import { StrictMode } from 'react'
-// import { createElement } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-
-import MyTitle from './MyTitle'
-import App from './App.jsx'
-
-const root = createRoot(document.getElementById('root'))
-
-function MyList() {
-  return (
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-    </ul>
-  )
-}
-
-root.render(
-  <main>
-    <MyTitle />
-    <MyList />
-  </main>
-)
+import { createRoot } from "react-dom/client";
+const root = createRoot(document.getElementById("root"));
