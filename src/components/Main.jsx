b09c45a3379449ca33a3c44b2c6a25a2f87a2b1e
COOLSTORM36 <yoyo.sunshine@hotmail.com>
2025-01-29 23:36:36 +0000

    #

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,20 @@
 export default function Main() {
     return (
-      <h1>App components here</h1>
+      <main>
+        <h1>
+          Fun facts about React
+        </h1>
+        <ul className="facts-list">
+          <li>
+            React is a library for building user interfaces
+          </li>
+          <li>
+            React can be used to build single-page applications
+          </li>
+          <li>
+            React allows you to create reusable UI components
+          </li>
+        </ul>
+      </main>
     )
   }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,14 @@
 export default function NavBar() {
     return (
       <header className='header'>
-        <img src="src/assets/react.svg" className="nav-logo" />
         <nav>
-          <ul className="nav-list">
+          <img src="src/assets/react.svg" />
+          <span>ReactFacts</span>
+          {/* <ul className="nav-list">
             <li className='nav-list-item'>Pricing</li>
             <li className='nav-list-item'>About</li>
             <li className='nav-list-item'>Contact</li>
-          </ul>
+          </ul> */}
         </nav>
       </header>
     )
