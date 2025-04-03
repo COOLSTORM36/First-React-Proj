a64c5b297e3bd455bd021ff07073beb1896b52e3
COOLSTORM36 <yoyo.sunshine@hotmail.com>
2025-04-03 16:54:42 +0100

    Minor Change

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,14 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { createElement } from "react";
 
 const root = createRoot(document.getElementById("root"));
+// const MyHeader = <h1>Welcome</h1>;
 
 root.render(
-    <App />
+    <div>
+        <App />
+    </div>
+  
+    // MyHeader
 );
